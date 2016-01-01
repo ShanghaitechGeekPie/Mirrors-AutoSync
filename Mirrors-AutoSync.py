d1954423c7a021488c275e1a76c9df654debd809
eastpiger <lvwt@shanghaitech.edu.cn>
2016-01-01 14:01:32 +0800

    update timezone

--- Mirrors-AutoSync.py
+++ Mirrors-AutoSync.py
@@ -90,7 +90,7 @@ class task(object):
 	def runner(self):
 		global rsynclock
 
-		while rsynclock >= 1 :
+		while rsynclock >= 2 :
 			print("	[{}] other job is running, wait for 1 minutes.\n".format(self.name))
 			time.sleep(60)
 
