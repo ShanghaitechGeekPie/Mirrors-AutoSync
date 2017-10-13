c62d3ec92268b3cb7be0c94bd97ed7ff1e510cf3
llk89 <shenxiang_1@126.com>
2017-10-13 23:50:13 +0800

    Integrated quick-fedora-mirror.
    Things need attention:
    1. I've slightly modified quick-fedora-mirror to make sure it can read some configuration from command line instead of its config, making universal config still viable. Its own config is still retained because it's still used to provide some can't-be-changing properties.
    2. Need to add a Volume named /metadata to hold time file.
    3. Modified quick-fedora-mirror is donated to public domain.
    4. EPEL mirror is NO LONGER RECIEVING SYNC because it's covered in fedora-buffet. We may consider drop it.
    5. I didn't add quick-fedora-hardlink because I heard we have more than plenty of disk space...
    
    Signed-off-by: llk89 <shenxiang_1@126.com>

---
+++ script/fedora.py
@@ -0,0 +1,66 @@
+# This script does not follow the general command line argument convention. 
+
+import sys
+import json
+import datetime, time
+import os
+import fcntl
+
+Name			=	sys.argv[1]
+LocalPath		=	sys.argv[2]
+RemotePath		=	sys.argv[3]
+TimefilePath	=	sys.argv[4]
+StatusPath		=	sys.argv[5]
+
+def writer(statuscode):
+	if not os.path.exists(StatusPath):
+		output_file = open(StatusPath, 'a+')
+		output_file.close()
+
+	output_file = open(StatusPath, 'r+')
+
+	fcntl.flock(output_file.fileno(), fcntl.LOCK_EX)
+
+	try:
+		content = json.loads(output_file.read())
+	except:
+		content = []
+
+	newstatus = {
+		'name': Name,
+		'statuscode': statuscode,
+		'time': time.time()
+	}
+
+	insert = True
+	for i in range(len(content)):
+		if content[i]['name'] == Name:
+			content[i] = newstatus
+			insert = False
+	if insert: content.append(newstatus)
+
+	content = sorted(content, key = lambda t:t['name'])
+
+	output_file.seek(0)
+
+	output_file.write(json.dumps(content))
+
+	output_file.truncate() 
+
+	output_file.close()
+
+command_literal = "./quick-fedora-mirror --timefile {} --destination-dir {} --upstream {} {}"
+		.format(TimefilePath, LocalPath, RemotePath, "" if os.path.exists(TimefilePath) else "-a")
+	
+print("	[{}] run command {}.".format(Name, command_literal))
+
+writer(-1)
+
+statuscode = os.system(command_literal) >> 8
+
+writer(statuscode)
+
+if statuscode != 0:
+	print("	[{}] failed with error code {}."
+				.format(Name, statuscode))
+	exit(233)
