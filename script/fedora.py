036f0b20908e73903e296867e93528a43ce44e30
zxt <zhaoxiting1997@gmail.com>
2017-10-28 14:51:34 +0800

    try fix quick-fedora-mirror

--- script/fedora.py
+++ script/fedora.py
@@ -49,7 +49,7 @@ def writer(statuscode):
 
 	output_file.close()
 
-command_literal = "/Mirrors-AutoSync/script/quick-fedora-mirror --timefile {} --destination-dir {} --upstream {} {}".format(TimefilePath, LocalPath, RemotePath, "" if os.path.exists(TimefilePath) else "-a")
+command_literal = "/Mirrors-AutoSync/script/quick-fedora-mirror -c /Mirrors-AutoSync/script/quick-fedora-mirror.conf --timefile {} --destination-dir {} --upstream {} {}".format(TimefilePath, LocalPath, RemotePath, "" if os.path.exists(TimefilePath) else "-a")
 
 print("	[{}] run command {}.".format(Name, command_literal))
 
