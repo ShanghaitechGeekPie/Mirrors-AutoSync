ba26b899506b59db5f8d271dfa69436f26fc4080
llk89 <27812632+llk89@users.noreply.github.com>
2018-10-31 11:29:43 +0800

    add infrastructure for sync that uses wget -m (#6)

---
+++ script/wget-m.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+from writer import writer
+
+Name		=	sys.argv[1]
+ConfigPath	=	sys.argv[2]
+StatusPath	=	sys.argv[3]
+RemotePath	=	sys.argv[4]
+LocalPath	=	sys.argv[5]
+
+command = '/Mirrors-AutoSync/script/wget-m.sh -u {} -d {} -R "html,css,jpg,jpeg,png,js" -x "*.1,*.html"'.format(RemotePath, LocalPath)
+
+print("	[{}] run command {}.".format(Name, command))
+
+writer(Name, ConfigPath, StatusPath, -1)
+
+statuscode = os.system(command) >> 8
+
+writer(Name, ConfigPath, StatusPath, statuscode)
+
+if statuscode != 0:
+	print("	[{}] failed with error code {}."
+				.format(Name, statuscode))
+exit(233)
---
+++ script/wget-m.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+# Wrapper around wget -m.
+#
+# Copyright (C) 2018 Jinrui Wang <wangjr@shanghaitech.edu.cn>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# TODO: Handle wget error and logs.
+
+usage() {
+    cat<<EOF
+$0: Invalid argument(s)
+Usage: $0 -u <url> -d <target> [-x <glob>] [--pre-release]
+Wrapper around wget -m.
+ARGUMENTS:
+  -u,  --upstream=<url>         A url to the root of the site. No parent will be fetched.
+  -d,  --target=<target>        The path to store the downloaded files locally. 
+  -R,  --reject                 A comma separated list of file extensions to reject. Passed to wget -R unchanged.
+  -x,  --remove                 A comma separated list of glob patterns to remove unwanted files, after wget -m finished.
+                                Normally only used by the script itself.
+       --pre-release            Fetch pre-releases. Default behavior is not fetch it. 
+LICENSING
+     Licensed under AGPL v3.0.
+AUTHOR
+    Jinrui Wang <wangjr@shanghaitech.edu.cn>
+EOF
+    exit 1
+}
+
+prepare() {
+    mkdir -p $TARGET
+    if [ $? -ne 0 ] ; then
+        echo "$0: Unable to create directories. Refer to above for error details. Aborting!"
+        exit 1
+    fi
+}
+
+do_fetch() {
+    wget -m "$UPSTREAM" -R "$EXCLUDE" -e "robots=off" -np -nH -P $TARGET
+}
+
+post_remove() {
+    for pattern in ${REMOVE//,/ }
+    do
+        find . -name "$pattern" | xargs -n1 -r -t rm
+    done
+}
+
+parse_arg() {
+    while [ $# -gt 0 ] ; do
+        case $1 in
+            -u|--upstream)
+                UPSTREAM=$2
+                shift 2
+                ;;
+            -d|--target)
+                TARGET=$2
+                shift 2
+                ;;
+            -R|--reject)
+                EXCLUDE=$2
+                shift 2
+                ;;
+            -x|--remove)
+                REMOVE=$2
+                shift 2
+                ;;
+            *)
+                usage
+                ;;
+        esac
+    done
+}
+
+parse_arg $*
+
+prepare
+do_fetch
+post_remove
